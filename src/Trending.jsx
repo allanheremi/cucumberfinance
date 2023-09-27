0dbf7e27891b9bac262b1fa4219b88a27fc641e8
Allan Heremi <allanheremidev@gmail.com>
2023-09-27 11:59:04 +0200

    Chore: Trending component WIP market data

---
+++ src/Trending.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Trending() {
+  return (
+    <div>
+        <div className='grid grid-cols-4 w-[12rem] list-none'>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+        </div>
+        <div className='grid grid-cols-4 w-[12rem] list-none'>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+        </div>
+        <div className='grid grid-cols-4 w-[12rem] list-none'>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+        </div>
+        <div className='grid grid-cols-4 w-[12rem] list-none'>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Trending
